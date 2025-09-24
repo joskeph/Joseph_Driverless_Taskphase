4d8a845b20f0b5eafba4459ac8965aaa894f13ea
Joseph Sony <joskeph@gmail.com>
2025-09-24 17:43:51 +0530

    commit add q1

---
+++ Task-1/q1.py
@@ -0,0 +1,7 @@
+'''Input an integer n. Input n number of strings and create a list of strings. Create a dictonary
+where key is an alphabet, and value is the number of �mes this alphabet is present in all the strings
+in the list (Not case sensitve, i.e count both uppercase and lowercase).
+For example: If the list is : ["Formula", "Manipal"], then the dic�onary output would look something
+like: {'f': 1, 'o':1, 'a': 3 ...'''
+
+print("Hello world")
